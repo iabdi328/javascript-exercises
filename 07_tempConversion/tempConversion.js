6d2e670dd5a594afdc364ed542d81eac070f037a
iabdi328 <ismailabdi328@gmail.com>
2024-07-27 20:12:32 -0400

    completed the assigned js tasks

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempToConvert) {
+  let celsius = Math.round((tempToConvert - 32) * (5 / 9) * 10) / 10;
+  return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempToConvert) {
+  let fahrenheit = Math.round(((tempToConvert * 9) / 5 + 32) * 10) / 10;
+  return fahrenheit;
 };
-
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
